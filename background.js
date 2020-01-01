a90df0ecb9ec8dabc77725b90067ecf592dbdab3
eltonlin <eltonlin@umich.edu>
2020-01-01 17:42:20 -0500

    init commit, prototype works - move first(two) tabs to a new window with keyboard command

---
+++ background.js
@@ -0,0 +1,56 @@
+
+// first create a new window and move selected tabs to it
+function move_to_new_window(tabs) {
+
+    chrome.windows.create({}, function(window) {
+
+        var new_wind_id = window.id;
+        var empty_tab_id = window.tabs[0].id;
+        console.log("empty tab id: ", empty_tab_id);
+
+        // append each tab to last index
+        chrome.tabs.move([tabs[0].id, tabs[1].id], {"index": -1, "windowId": new_wind_id}, function(){
+
+            //after inserted all tabs, remove empty tab
+            chrome.tabs.remove(empty_tab_id, function(){});
+
+            // chrome.tabs.query({"windowId": window_id}, function(tabs) {
+
+            //     tabs.forEach(function(t){
+            //         console.log("tabs: ", t.id, t.title);
+            //     });
+            //     //chrome.tab.remove(tabs[0].id, function(){});
+            // });
+        });
+
+        // could close the empty tab, window.udpate...
+        // https://stackoverflow.com/questions/19972843/chrome-extension-to-move-multiple-tabs-to-new-window
+    });
+
+    
+}
+
+// prototype: triggered by keyboard command
+// next is to enable UI elements and click trigger
+chrome.commands.onCommand.addListener(function(command) {
+    if(command == "move-to-new-window") {
+        console.log('moving...');
+
+        chrome.tabs.query({"currentWindow": true}, function(tabs){
+
+            
+            var tabs_to_move = tabs;
+            move_to_new_window(tabs_to_move);
+
+            // var idx = tabs[1].id;
+            // chrome.tabs.move(tabs[0].id, {"index": -1}, function(){
+            //     console.log("index: ", idx, tabs[1].id);
+            //     chrome.tabs.remove(tabs[1].id, function(){});
+            // });
+            
+        });
+
+
+    }
+    
+});
